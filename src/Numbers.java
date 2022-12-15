9a3f349a0fa625980f258c5fae7dc4ce601a68dd
Marko Mlakić <113576925+markomlakic@users.noreply.github.com>
2022-12-15 08:26:20 +0100

    Task created!

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,2 +1,34 @@
 public class Numbers {
+
+    public static void main(String[] args) {
+
+        int num = 7;
+
+        if (num == 0) {
+            System.out.println("Selected number is zero");
+        } else if (num == 1) {
+            System.out.println("Selected number is one");
+        } else if (num == 2) {
+            System.out.println("Selected number is two");
+        } else if (num == 3) {
+            System.out.println("Selected number is three");
+        } else if (num == 4) {
+            System.out.println("Selected number is four");
+        } else if (num == 5) {
+            System.out.println("Selected number is five");
+        } else if (num == 6) {
+            System.out.println("Selected number is six");
+        } else if (num == 7) {
+            System.out.println("Selected number is seven");
+        } else if (num == 8) {
+            System.out.println("Selected number is eight");
+        } else if (num == 9) {
+            System.out.println("Selected number is nine");
+        } else {
+            System.out.println("error");
+        }
+    }
 }
+/*
+Write a program that can convert numbers 0~9 to words on Numbers class ( input 0-->output zero)
+ */
