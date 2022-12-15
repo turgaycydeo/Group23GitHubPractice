75082652698984c4ff3453b70c5f0356b966788f
IgorStrangarevic <igor.strangarevic@outlook.com>
2022-12-15 08:25:27 +0100

    added numbers to letters code in Numbers class

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,2 +1,33 @@
 public class Numbers {
+
+    public static void main(String[] args) {
+
+        int num = 0;
+
+        if (num==0){
+            System.out.println("Zero");
+        } else if (num==1) {
+            System.out.println("One");
+        } else if (num==2) {
+            System.out.println("Two");
+        } else if (num==3) {
+            System.out.println("Three");
+        } else if (num==4) {
+            System.out.println("Four");
+        } else if (num==5) {
+            System.out.println("Five");
+        } else if (num==6) {
+            System.out.println("Six");
+        } else if (num==7) {
+            System.out.println("Seven");
+        } else if (num==8) {
+            System.out.println("Eight");
+        } else if (num==9) {
+            System.out.println("Nine");
+        }
+
+
+    }
+
+
 }
